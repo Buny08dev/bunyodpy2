0b8a9ec2976d323d46611111c9aa01c4f97d06f4
Buny08dev <goldazamat08@gmail.com>
2026-02-04 10:17:38 +0300

    login

--- bun_django/bun_users/urls.py
+++ bun_django/bun_users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from bun_users.views import Registeview
+from bun_users.views import Registerview
 
 urlpatterns = [
-    path("",Registeview.as_view(),name="login"),]
+    path("",Registerview.as_view(),name="login"),]
